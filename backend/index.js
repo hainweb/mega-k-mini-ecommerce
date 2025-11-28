77002fd4437fb6cb8031dd35d43a7128deb43b98
hain <hainofficialweb@gmail.com>
2025-11-28 21:56:11 +0530

    fix: add cache and update cors

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,32 @@
-
 import cors from 'cors';
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from './config/db.js';
 import productRouters from './routes/productRouters.js';
-dotenv.config();
 
-connectDB()
+dotenv.config();
+connectDB();
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(cors());
-app.use(cors({
+
+const corsOptions = {
   origin: "https://mega-k-mini-ecommerce-cv4q.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+  allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Pragma"],
+};
 
-//.use('/api/upload', express.static('public/uploads'));
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("Server is running!");
 });
 
-app.use('/api', productRouters)
+app.use('/api', productRouters);
 
-
-const PORT =  5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
-
