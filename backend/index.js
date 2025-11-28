c2a816c4e978ecefbf0a570d942b103d799cf5ea
hain <hainofficialweb@gmail.com>
2025-11-28 22:09:47 +0530

    fix: remove cors blocking

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,27 @@
+
 import cors from 'cors';
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from './config/db.js';
 import productRouters from './routes/productRouters.js';
-
 dotenv.config();
-connectDB();
 
+connectDB()
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-const corsOptions = {
-  origin: "https://mega-k-mini-ecommerce-cv4q.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Pragma"],
-};
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+//app.use(cors());
+app.use(cors());
+//.use('/api/upload', express.static('public/uploads'));
 
 app.get("/", (req, res) => {
   res.send("Server is running!");
 });
 
-app.use('/api', productRouters);
+app.use('/api', productRouters)
 
-const PORT = 5000;
+
+const PORT =  5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
